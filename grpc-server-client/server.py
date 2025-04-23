848281811db30139dd8236500ef87f1287510ed5
paramkansagra <param.j.kansagra@gmail.com>
2025-04-23 22:58:16 +0530

    created the server

---
+++ grpc-server-client/server.py
@@ -0,0 +1,41 @@
+from concurrent import futures
+
+from typing import List
+
+import grpc
+import employee_pb2
+import employee_pb2_grpc
+
+
+class EmployeeGRPCServicer(employee_pb2_grpc.EmployeeGRPCServicer):
+    def __init__(self):
+        self.employees: List[employee_pb2.Employees] = []
+        self.id = 1
+
+    def addEmployee(self, request: employee_pb2.Employee, context):
+        request.id = self.id
+
+        self.employees.append(request)
+        self.id += 1
+
+        return request
+
+    def getEmployees(self, request: employee_pb2.void, context):
+        return self.employees
+
+
+def run():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+    employee_pb2_grpc.add_EmployeeGRPCServicer_to_server(EmployeeGRPCServicer(), server)
+
+    server.add_insecure_port("[::]:6969")
+    server.start()
+
+    print("starting server at port 6969")
+
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    run()
